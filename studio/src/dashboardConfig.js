9d387f3513e497272d7225d4779f118ed6c3b95f
Casey K <tehcnicolour@gmail.com>
2021-04-12 11:52:17 -0700

    deplpoy button

---
+++ studio/src/dashboardConfig.js
@@ -0,0 +1,27 @@
+export default {
+  widgets: [
+    {
+      name: "sanity-tutorials"
+    },
+    {
+      name: "project-info"
+    },
+    {
+      name: "project-users"
+    },
+    {
+      name: "netlify",
+      options: {
+        title: "My Netlify deploys",
+        sites: [
+          {
+            title: "Cahuilla Tribe",
+            apiId: "8eef4c51-ad30-48f9-9dd4-169a5d568132",
+            buildHookId: "6074961f8f8b9400d800a753",
+            name: "cahuillatribe"
+          }
+        ]
+      }
+    }
+  ]
+};
